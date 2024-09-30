342a87fc8db8058c40fab48e07e9ca9e3bdf0626
Shibo Lyu <github@of.sb>
2024-09-30 16:21:19 +0800

    fix: forgot to export identity class 🤦

--- identity/mod.ts
+++ identity/mod.ts
@@ -1,3 +1,4 @@
 export type { BlahActKeyRecord } from "./actKey.ts";
 export type { BlahIdentityFile } from "./identityFile.ts";
 export type { BlahProfile } from "./profile.ts";
+export * from "./identity.ts";
