d31970be784865bdf605bb32d9d1911daeeee19e
Shibo Lyu <hi@lao.sb>
2024-12-06 00:17:46 +0800

    feat: expose actkey expires at

--- identity/actKey.ts
+++ identity/actKey.ts
@@ -98,6 +98,10 @@ export class BlahActKey {
     );
   }
 
+  get expiresAt(): Date {
+    return this.internalExpiresAt;
+  }
+
   get isExpired(): boolean {
     return new Date() > this.internalExpiresAt;
   }
