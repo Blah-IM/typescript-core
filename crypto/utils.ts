598ed931e0b20be914202f1eb82fd4a526192ee9
Shibo Lyu <hi@lao.sb>
2025-03-11 19:03:48 +0800

    fix: array buffer typing

--- crypto/utils.ts
+++ crypto/utils.ts
@@ -1,5 +1,6 @@
-export function bufToHex(buf: ArrayBufferLike): string {
-  return [...new Uint8Array(buf)].map((x) => x.toString(16).padStart(2, "0"))
+export function bufToHex(buf: ArrayBufferLike | Uint8Array): string {
+  const u8Array = buf instanceof Uint8Array ? buf : new Uint8Array(buf);
+  return [...u8Array].map((x) => x.toString(16).padStart(2, "0"))
     .join("");
 }
 
