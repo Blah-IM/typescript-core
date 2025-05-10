1e2897974f1d9f069e34e4ee98b3b3d49322e8e3
Shibo Lyu <hi@lao.sb>
2025-05-11 02:21:31 +0800

    fix: Add JSDoc and export signAndVerify module

--- crypto/mod.ts
+++ crypto/mod.ts
@@ -1,4 +1,11 @@
+/**
+ * `crypto` provides cryptographic operations and utilities for the Blah protocol, including key pairs and signing.
+ *
+ * @module
+ */
+
 export * from "./keypair.ts";
 export * from "./publicKey.ts";
 export type { BlahPayloadSignee, BlahSignedPayload } from "./signedPayload.ts";
+export * from "./signAndVerify.ts";
 export * from "./utils.ts";
